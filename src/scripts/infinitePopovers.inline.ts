57c966d15792e61599d77963ef99612673f13d04
SongCorgi <zsong2308@gmail.com>
2026-07-02 22:13:38 +0800

    v1.0

--- src/scripts/infinitePopovers.inline.ts
+++ src/scripts/infinitePopovers.inline.ts
@@ -44,7 +44,7 @@ let activeAnchor: HTMLAnchorElement | null = null
 
 let currentActivePopoverNode: PopoverNode | null = null
 
-const ALIVE_REGION_DEBOUNCE = 300
+const ALIVE_REGION_DEBOUNCE = 750
 
 const popovers = new Map<string, PopoverNode>()
 
